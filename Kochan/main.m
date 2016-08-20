d3c3821f12f2767c0f632dd08ac3e1b563ba0132
Dean Silfen <dean.silfen@gmail.com>
2016-08-20 13:19:11 -0400

    Initial Commit

---
+++ Kochan/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Kochan
+//
+//  Created by Dean Silfen on 8/20/16.
+//  Copyright © 2016 Dean Silfen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
